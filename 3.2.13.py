5a868519885ac20fc9b963efdf1acc61e3c85780
Denis Mordvinov <103065243+Dis2k4@users.noreply.github.com>
2022-06-23 20:54:12 +0400

    3.2.13 homework

---
+++ 3.2.13.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.first")
+        input1.send_keys("First")
+        input2 = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.second")
+        input2.send_keys("Second")
+        input3 = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.third")
+        input3.send_keys("Third")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        time.sleep(10)
+        browser.quit()
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be absolute value of a number")
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.first")
+        input1.send_keys("First")
+        input2 = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.second")
+        input2.send_keys("Second")
+        input3 = browser.find_element(By.CSS_SELECTOR, "div.first_block input.form-control.third")
+        input3.send_keys("Third")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        time.sleep(10)
+        browser.quit()
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be absolute value of a number")
+
+if __name__ == "__main__":
+    unittest.main()
